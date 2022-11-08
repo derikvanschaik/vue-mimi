7899091fbfabb12de8b5fa2dcc8e8d0ecda3b630
Derik van Schaik <derikvanschaik@gmail.com>
2022-11-08 13:30:18 -0800

    multiple draggable components with a canvas element in background

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,33 @@
 <template>
-  <div 
-    :key="i"
-    class="textbox"
-    @mousemove="handleDrag"
-    @mousedown="handleDragStart"
-    @mouseup="handleDragEnd">
-    {{ text }}
-  </div>
+    <canvas :width="width" :height="height"></canvas>
+    <div v-for="i in [1, 2, 3]" :key="i">
+        <drag-text/>
+    </div>
 </template>
 
 <script>
+import DragText from './components/DragText.vue';
 
 export default {
-  name: 'App',
-  data(){
-    return{
-      text: 'hello world',
-      x: 0,
-      y: 0,
-      isDragging: false
+    components:{DragText},
+    mounted(){
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+    }, 
+    data(){
+        return{
+            width: 0,
+            height: 0
+        }
     }
-  },
-  methods: {
-    handleDragStart(e){
-      // set the current mouse position
-      this.x = e.clientX;
-      this.y = e.clientY;
-      this.isDragging = true;
-    },
-    handleDrag(e){
-      if(this.isDragging){
-
-        const dx = e.clientX - this.x;
-        const dy = e.clientY - this.y;
-
-        // Set the position of element
-        e.target.style.top = `${e.target.offsetTop + dy}px`;
-        e.target.style.left = `${e.target.offsetLeft + dx}px`;
-
-        // Reassign the position of mouse
-        this.x = e.clientX;
-        this.y = e.clientY;
-      }
-    },
-    handleDragEnd(){
-      this.isDragging = false;
-    }
-  }
 }
 </script>
 <style>
-* {
-  margin: 0;
-  padding: 0;
+*{
+    margin: 0;
+    padding:0
 }
-.textbox{
-  background-color: grey;
-  position: absolute;
-  padding: 50px 50px;
-  cursor: pointer;
+canvas{
+    position: absolute;
 }
 </style>
---
+++ src/components/DragText.vue
@@ -0,0 +1,61 @@
+<template>
+  <div
+    class="textbox"
+    @mousemove="handleDrag"
+    @mousedown="handleDragStart"
+    @mouseup="handleDragEnd">
+    {{ text }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      text: 'hello world',
+      x: 0,
+      y: 0,
+      isDragging: false
+    }
+  },
+  methods: {
+    handleDragStart(e){
+      // set the current mouse position
+      this.x = e.clientX;
+      this.y = e.clientY;
+      this.isDragging = true;
+    },
+    handleDrag(e){
+      if(this.isDragging){
+
+        const dx = e.clientX - this.x;
+        const dy = e.clientY - this.y;
+
+        // Set the position of element
+        e.target.style.top = `${e.target.offsetTop + dy}px`;
+        e.target.style.left = `${e.target.offsetLeft + dx}px`;
+
+        // Reassign the position of mouse
+        this.x = e.clientX;
+        this.y = e.clientY;
+      }
+    },
+    handleDragEnd(){
+      this.isDragging = false;
+    }
+  }
+}
+</script>
+<style>
+.textbox{
+  background-color: grey;
+  position: absolute;
+  padding: 50px 50px;
+  cursor: pointer;
+  user-select: none;
+  color: white;
+  font-size: x-large;
+}
+</style>
