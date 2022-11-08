3d14f24cf3173bdba8b99f7e6f66ddcbb904210f
Derik van Schaik <derikvanschaik@gmail.com>
2022-11-08 13:03:40 -0800

    implement draggable component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div 
+    :key="i"
+    class="textbox"
+    @mousemove="handleDrag"
+    @mousedown="handleDragStart"
+    @mouseup="handleDragEnd">
+    {{ text }}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      text: 'hello world',
+      x: 0,
+      y: 0,
+      isDragging: false
+    }
+  },
+  methods: {
+    handleDragStart(e){
+      // set the current mouse position
+      this.x = e.clientX;
+      this.y = e.clientY;
+      this.isDragging = true;
+    },
+    handleDrag(e){
+      if(this.isDragging){
+
+        const dx = e.clientX - this.x;
+        const dy = e.clientY - this.y;
+
+        // Set the position of element
+        e.target.style.top = `${e.target.offsetTop + dy}px`;
+        e.target.style.left = `${e.target.offsetLeft + dx}px`;
+
+        // Reassign the position of mouse
+        this.x = e.clientX;
+        this.y = e.clientY;
+      }
+    },
+    handleDragEnd(){
+      this.isDragging = false;
+    }
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+}
+.textbox{
+  background-color: grey;
+  position: absolute;
+  padding: 50px 50px;
+  cursor: pointer;
 }
 </style>
