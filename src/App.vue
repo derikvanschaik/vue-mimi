a4fddf47d7dc8a4fcfb9a244dca4d3795830c2ec
Derik van Schaik <derikvanschaik@gmail.com>
2022-11-08 16:08:00 -0800

    implement line drawing

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-    <canvas :width="width" :height="height"></canvas>
-    <div v-for="i in [1, 2, 3]" :key="i">
-        <drag-text/>
+    <canvas :width="width" :height="height" ref="canvas"></canvas>
+    <div v-for="t in textboxes" :key="t.idx">
+        <drag-text :updateLinesHandler="updateLines" :startX="10" :startY="400"/>
     </div>
 </template>
 
@@ -10,14 +10,54 @@ import DragText from './components/DragText.vue';
 
 export default {
     components:{DragText},
+    // TODO: maybe move this canvas resizing into a specific canvas component since it is not 
+    // not really app logic...
     mounted(){
-        this.width = window.innerWidth;
-        this.height = window.innerHeight;
-    }, 
+        this.clearCanvasAndDrawLines();
+    },
     data(){
         return{
-            width: 0,
-            height: 0
+            // canvas
+            width: window.innerWidth,
+            height: window.innerHeight,
+            // app 
+            textboxes: [
+                { id: 1 }
+            ],
+            lines: [ 
+                { fromX: 10, fromY: 400, toX: 10, toY: 0},
+            ],
+
+        }
+    },
+    methods: {
+        clearCanvasAndDrawLines(){
+            const ctx = this.$refs.canvas.getContext("2d");
+            ctx.clearRect(0, 0, this.width, this.height);
+
+            this.lines.forEach( line =>{
+                ctx.beginPath();
+                ctx.moveTo(line.fromX, line.fromY);
+                ctx.lineTo(line.toX, line.toY);
+                ctx.stroke();
+            })
+        },
+        // so far only redraws -- may need to add and delete lines in the future as well
+        // maybe we can add a final param that is a string describing the action 
+        // like: updateLines(oldX, oldY, newX, newY, action="DELETE")
+        // or we can update and check the param count..
+        updateLines(oldX, oldY, newX, newY){
+            for(let i = 0; i < this.lines.length; i++){
+                const {fromX, fromY, toX, toY} = this.lines[i];
+
+                if(fromX === oldX && fromY === oldY){
+                    this.lines[i] = {fromX: newX, fromY: newY, toX, toY};
+
+                }else if (toX === oldX && toY === oldY){
+                    this.lines[i] = {fromX, fromY, toX: newX, toY: newY};
+                }
+            }
+            this.clearCanvasAndDrawLines();
         }
     }
 }
--- src/components/DragText.vue
+++ src/components/DragText.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    ref="textbox"
     class="textbox"
     @mousemove="handleDrag"
     @mousedown="handleDragStart"
@@ -12,15 +13,26 @@
 
 export default {
   name: 'App',
+  props:{
+    updateLinesHandler: Function,
+    startX: Number,
+    startY: Number
+  }, 
+  mounted(){
+    // Set the position of element
+    this.$refs.textbox.style.left = `${this.startX}px`;
+    this.$refs.textbox.style.top = `${this.startY}px`;
+  }, 
   data(){
     return{
       text: 'hello world',
-      x: 0,
-      y: 0,
+      x: this.startX,
+      y: this.startY,
       isDragging: false
     }
   },
   methods: {
+
     handleDragStart(e){
       // set the current mouse position
       this.x = e.clientX;
@@ -29,10 +41,13 @@ export default {
     },
     handleDrag(e){
       if(this.isDragging){
-
+        // draw textbox 
         const dx = e.clientX - this.x;
         const dy = e.clientY - this.y;
 
+        // get old pos 
+        const oldOffsetTop = e.target.offsetTop;
+        const oldOffsetLeft = e.target.offsetLeft;
         // Set the position of element
         e.target.style.top = `${e.target.offsetTop + dy}px`;
         e.target.style.left = `${e.target.offsetLeft + dx}px`;
@@ -40,6 +55,12 @@ export default {
         // Reassign the position of mouse
         this.x = e.clientX;
         this.y = e.clientY;
+        // call callback
+        
+        // values are left and top for x and y -- is this what we want though?...
+        this.updateLinesHandler(
+          oldOffsetLeft, oldOffsetTop,
+          (e.target.offsetLeft),(e.target.offsetTop));
       }
     },
     handleDragEnd(){
