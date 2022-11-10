fd86967ea3774784f4ec1e839c82f1be293b238c
Derik van Schaik <derikvanschaik@gmail.com>
2022-11-10 15:20:48 -0800

    have app level and move app logic into mindmap component

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,18 @@
 <template>
-    <canvas :width="width" :height="height" ref="canvas"></canvas>
-    <div class="menu">
-        <button class="menu-btn" @click="addTextbox">Add Tbox</button>
-        <button class="menu-btn" @click="connectedSelected" :disabled="cannotConnect">Connect</button>
-    </div>
-    <drag-text
-        v-for="t,i in textboxes"
-        :key="t.id"
-        :updateLinesHandler="updateLines"
-        :updateTextbox="updateTextbox(i)"
-        :deleteTextbox="deleteTextbox(i)"
-        :selectTextbox="selectTextbox(i)"
-        :updateTextboxText="updateTextboxText(i)"
-        :selected="t.selected"
-        :text="t.text"
-        :startX="t.x" 
-        :startY="t.y"/>
+    <mind-map :curTextboxes="textboxes" :curLines="lines" />
 </template>
 
 <script>
-import DragText from './components/DragText.vue';
+import MindMap from './components/MindMap.vue';
 const { v4: uuid } = require("uuid")
 
 export default {
-    components:{DragText},
-    // TODO: maybe move this canvas resizing into a specific canvas component since it is not 
-    // not really app logic...
-    mounted(){
-        this.clearCanvasAndDrawLines();
-    },
-    computed:{
-        cannotConnect(){
-            return this.textboxes.filter(t => t.selected).length !== 2;
-        }
-    },
-    data(){
-        return{
-            // canvas
-            width: window.innerWidth,
-            height: window.innerHeight,
-            // app 
-            textboxes: [
+  components:{
+    MindMap
+  },
+  data(){
+    return{
+        textboxes: [
                 { x: 10, y: 100, text: 'hello', selected: false, id: uuid() },
                 { x: 100, y: 500, text: 'world', selected: false, id: uuid() },
                 { x: 300, y: 600, text: 'how are you?', selected: false, id: uuid() }
@@ -49,108 +21,7 @@ export default {
                 { fromX: 10, fromY: 100, toX: 100, toY: 500},
                 { fromX: 10, fromY: 100, toX: 300, toY: 600}
             ],
-
-        }
-    },
-    methods: {
-        addTextbox(){
-            this.textboxes.push(
-                {
-                    x: 500, 
-                    y: 500,
-                    text: 'click edit to change text',
-                    selected: false,
-                    id: uuid()
-                }
-            )
-        },
-        selectTextbox(idx){
-            return () =>{
-                this.textboxes[idx].selected = !this.textboxes[idx].selected;
-            }
-        },
-        deleteTextbox(idx){
-            return () =>{
-                this.textboxes = this.textboxes.filter( (_, i) => i !== idx);
-            }
-        },
-        updateTextbox(idx){
-            return (x, y) =>{
-                this.textboxes[idx] = { ...this.textboxes[idx],x, y };
-            }
-        },
-        updateTextboxText(idx){
-            return (text) =>{
-                this.textboxes[idx] = {...this.textboxes[idx], text};
-            }
-        },
-        clearCanvasAndDrawLines(){
-            const ctx = this.$refs.canvas.getContext("2d");
-            ctx.clearRect(0, 0, this.width, this.height);
-
-            this.lines.forEach( line =>{
-                // console.log(`drawing line from (${line.fromX},${line.fromY}) to (${line.toX},${line.toY})` );
-                ctx.beginPath();
-                ctx.moveTo(line.fromX, line.fromY);
-                ctx.lineTo(line.toX, line.toY);
-                ctx.stroke();
-            })
-        },
-        // so far only redraws -- may need to add and delete lines in the future as well
-        // maybe we can add a final param that is a string describing the action 
-        // like: updateLines(action="DELETE", oldX, oldY, newX, newY)
-        // or we can update and check the param count..
-        updateLines(oldX, oldY, newX, newY){
-            const deletingLine = (newX === null || newX === undefined) 
-            && (newY === null || newY === undefined);
-            
-            for(let i = 0; i < this.lines.length; i++){
-                const {fromX, fromY, toX, toY} = this.lines[i];
-
-                if(fromX === oldX && fromY === oldY){
-                    if(deletingLine){
-                        this.lines[i] = null; // placeholder to later delete
-                    }else{
-                        this.lines[i] = {fromX: newX, fromY: newY, toX, toY};
-                    }
-
-                }else if (toX === oldX && toY === oldY){
-                    if(deletingLine){
-                        this.lines[i] = null; // placeholder to delete line
-                    }else{
-                        this.lines[i] = {fromX, fromY, toX: newX, toY: newY};
-                    }
-                }
-            }
-            this.lines = this.lines.filter( line => line !== null); // filter deleted lines
-            this.clearCanvasAndDrawLines();
-        },
-        connectedSelected(){
-            const [b1, b2] = this.textboxes.filter( t => t.selected);
-            const newLine = { fromX: b1.x, fromY: b1.y, toX: b2.x, toY: b2.y};
-            this.lines.push(newLine);
-            this.clearCanvasAndDrawLines();
-            // deselect all post connection
-            this.textboxes.forEach( t => t.selected = false);
-        }
     }
+  }
 }
 </script>
-<style>
-*{
-    margin: 0;
-    padding:0
-}
-canvas{
-    position: absolute;
-    background-color: #DCDCDC;
-}
-.menu{
-    position: absolute;
-    display: flex;
-    align-items: center;
-}
-.menu-btn{
-    padding: 10px 15px;
-}
-</style>
---
+++ src/components/MindMap.vue
@@ -0,0 +1,152 @@
+<template>
+    <canvas :width="width" :height="height" ref="canvas"></canvas>
+    <div class="menu">
+        <button class="menu-btn" @click="addTextbox">Add Tbox</button>
+        <button class="menu-btn" @click="connectedSelected" :disabled="cannotConnect">Connect</button>
+    </div>
+    <drag-text
+        v-for="t,i in textboxes"
+        :key="t.id"
+        :updateLinesHandler="updateLines"
+        :updateTextbox="updateTextbox(i)"
+        :deleteTextbox="deleteTextbox(i)"
+        :selectTextbox="selectTextbox(i)"
+        :updateTextboxText="updateTextboxText(i)"
+        :selected="t.selected"
+        :text="t.text"
+        :startX="t.x" 
+        :startY="t.y"/>
+</template>
+
+<script>
+import DragText from './DragText.vue';
+const { v4: uuid } = require("uuid")
+
+export default {
+    components:{DragText},
+    props:{
+        curTextboxes: Array,
+        curLines: Array,
+    }, 
+    // TODO: maybe move this canvas resizing into a specific canvas component since it is not 
+    // not really app logic...
+    mounted(){
+        this.clearCanvasAndDrawLines();
+    },
+    computed:{
+        cannotConnect(){
+            return this.textboxes.filter(t => t.selected).length !== 2;
+        }
+    },
+    data(){
+        return{
+            // canvas
+            width: window.innerWidth,
+            height: window.innerHeight,
+            // app 
+            textboxes: this.curTextboxes,
+            lines: this.curLines,
+        }
+    },
+    methods: {
+        addTextbox(){
+            this.textboxes.push(
+                {
+                    x: 500, 
+                    y: 500,
+                    text: 'click edit to change text',
+                    selected: false,
+                    id: uuid()
+                }
+            )
+        },
+        selectTextbox(idx){
+            return () =>{
+                this.textboxes[idx].selected = !this.textboxes[idx].selected;
+            }
+        },
+        deleteTextbox(idx){
+            return () =>{
+                this.textboxes = this.textboxes.filter( (_, i) => i !== idx);
+            }
+        },
+        updateTextbox(idx){
+            return (x, y) =>{
+                this.textboxes[idx] = { ...this.textboxes[idx],x, y };
+            }
+        },
+        updateTextboxText(idx){
+            return (text) =>{
+                this.textboxes[idx] = {...this.textboxes[idx], text};
+            }
+        },
+        clearCanvasAndDrawLines(){
+            const ctx = this.$refs.canvas.getContext("2d");
+            ctx.clearRect(0, 0, this.width, this.height);
+
+            this.lines.forEach( line =>{
+                // console.log(`drawing line from (${line.fromX},${line.fromY}) to (${line.toX},${line.toY})` );
+                ctx.beginPath();
+                ctx.moveTo(line.fromX, line.fromY);
+                ctx.lineTo(line.toX, line.toY);
+                ctx.stroke();
+            })
+        },
+        // so far only redraws -- may need to add and delete lines in the future as well
+        // maybe we can add a final param that is a string describing the action 
+        // like: updateLines(action="DELETE", oldX, oldY, newX, newY)
+        // or we can update and check the param count..
+        updateLines(oldX, oldY, newX, newY){
+            const deletingLine = (newX === null || newX === undefined) 
+            && (newY === null || newY === undefined);
+            
+            for(let i = 0; i < this.lines.length; i++){
+                const {fromX, fromY, toX, toY} = this.lines[i];
+
+                if(fromX === oldX && fromY === oldY){
+                    if(deletingLine){
+                        this.lines[i] = null; // placeholder to later delete
+                    }else{
+                        this.lines[i] = {fromX: newX, fromY: newY, toX, toY};
+                    }
+
+                }else if (toX === oldX && toY === oldY){
+                    if(deletingLine){
+                        this.lines[i] = null; // placeholder to delete line
+                    }else{
+                        this.lines[i] = {fromX, fromY, toX: newX, toY: newY};
+                    }
+                }
+            }
+            this.lines = this.lines.filter( line => line !== null); // filter deleted lines
+            this.clearCanvasAndDrawLines();
+        },
+        connectedSelected(){
+            const [b1, b2] = this.textboxes.filter( t => t.selected);
+            const newLine = { fromX: b1.x, fromY: b1.y, toX: b2.x, toY: b2.y};
+            this.lines.push(newLine);
+            this.clearCanvasAndDrawLines();
+            // deselect all post connection
+            this.textboxes.forEach( t => t.selected = false);
+        }
+    }
+}
+</script>
+<style>
+*{
+    margin: 0;
+    padding:0
+}
+canvas{
+    position: absolute;
+    background-color: #DCDCDC;
+}
+.menu{
+    position: absolute;
+    display: flex;
+    align-items: center;
+}
+.menu-btn{
+    padding: 10px 15px;
+}
+</style>
