071a726ea6aa782afe7a385baa56a8a1e72327b0
Derik van Schaik <derikvanschaik@gmail.com>
2023-01-07 12:03:50 -0800

    more trial + error for github pages

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/vue-mimi/'
+//     : '/'
+// })
+module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-mimi/'
+    ? '/my-project/'
     : '/'
-})
+}
